f4497e20b0f200a7dbb59e8af8a1665eb429e7f8
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-04 23:52:54 +0900

    add shaping

--- examine/shaping.rb
+++ examine/shaping.rb
@@ -2,8 +2,17 @@
 require 'csv'
 Encoding.default_external = 'UTF-8'
 
-File.open("./hirakata.csv") do |f|
-  f.each do |row|
-    row = row.split(',')
+def shaping(name, sjis, utf8)
+  out = File.open("./#{name}_s.csv", "w")
+  File.open("./#{name}.csv") do |f|
+    f.each do |row|
+      row = row.split(',')
+      if row.length > 2 then
+        out.write(row[sjis] + "," + row[utf8] + "\n")
+      end
+    end
   end
 end
+
+shaping("hirakata", 1, 3)
+shaping("kanji", 4, 6)
