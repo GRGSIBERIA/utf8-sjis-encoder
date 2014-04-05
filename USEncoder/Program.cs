c0ddc9e4a55082cba6eab467cd43f2b6dd9adf2c
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-06 02:57:07 +0900

    barusu

--- USEncoder/Program.cs
+++ USEncoder/Program.cs
@@ -12,47 +12,8 @@ namespace USEncoder
 
         static void Main(string[] args)
         {
-            TestToSJIS();
-            TestToUTF8();
+            
         }
 
-        static string ConvertEncoding(Encoding dest, string str)
-        {
-            byte[] str_bytes = Encoding.Unicode.GetBytes(str);
-            byte[] to_bytes = Encoding.Convert(Encoding.Unicode, dest, str_bytes);
-            return dest.GetString(to_bytes);
-        }
-
-        static void Check(byte[] src, byte[] clone)
-        {
-            Console.WriteLine("{0}, {1}", src.Length, clone.Length);
-            for (int i = 0; i < src.Length; i++)
-            {
-                Console.WriteLine("{0}, {1}, {2}", src[i], clone[i], src[i] == clone[i]);
-            }
-            Console.WriteLine("--------------------------");
-        }
-
-        static void TestToUTF8()
-        {
-            var sjis = ConvertEncoding(Encoding.GetEncoding(932), str);
-            var sjis_bytes = Encoding.GetEncoding(932).GetBytes(sjis);
-            var utf8 = ToEncoding.ToUTF8(sjis_bytes);
-            var utf8_bytes = Encoding.UTF8.GetBytes(utf8);
-            var clone = ConvertEncoding(Encoding.UTF8, str);
-            var clone_bytes = Encoding.UTF8.GetBytes(clone);
-
-            Check(utf8_bytes, clone_bytes);
-        }
-
-        static void TestToSJIS()
-        {
-            var utf8 = ConvertEncoding(Encoding.UTF8, str);
-            var sjis_bytes = ToEncoding.ToSJIS(utf8);
-            var clone = ConvertEncoding(Encoding.GetEncoding(932), str);
-            var clone_bytes = Encoding.GetEncoding(932).GetBytes(clone);
-
-            Check(sjis_bytes, clone_bytes);
-        }
     }
 }
