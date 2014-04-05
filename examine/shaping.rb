c95d998dbdda3f92464a4f16f4410a75c7d23113
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-06 01:40:49 +0900

    make utf16 and sjis csv

--- examine/shaping.rb
+++ examine/shaping.rb
@@ -21,7 +21,7 @@ def writecs(out, path, key, val)
     f.each do |row|
       row = row.chomp.split(',')
       unless row.include?("------") then
-        out.write("\{0x#{row[key]}, 0x#{row[val]}\},\n")
+        out.write("0x#{row[key]}, 0x#{row[val]}\n")
       end
     end
   end
@@ -33,8 +33,33 @@ def makecs(path, key, val)
   writecs(out, "kanji", key, val)
 end
 
-shaping("hirakata", 1, 3)
-shaping("kanji", 4, 6)
+def toarray(path)
+  out = File.open("./#{path}_a.cs", "w")
+  hash = Hash.new
+  File.open("./#{path}.cs") do |f|
+    f.each do |row|
+      row = row.chomp.split(',')
+      a = row[0].hex
+      b = row[1].hex
+      hash[a] = b
+    end
+  end
+
+  for i in 0..65536 do 
+    buf = hash[i]
+    if buf == nil then
+      out.write("0,")
+    else
+      out.write("#{buf},")
+    end
+  end
+end
+
+shaping("hirakata", 1, 4)
+shaping("kanji", 4, 7)
 
 makecs("to_utf8", 0, 1)
 makecs("to_sjis", 1, 0)
+
+toarray("to_utf8")
+toarray("to_sjis")
