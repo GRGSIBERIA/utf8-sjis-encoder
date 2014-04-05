8e89f9f71b3f4640ce57bc15ad3500b4f18e876c
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-05 18:41:41 +0900

    complete Testing

--- USEncoder/Program.cs
+++ USEncoder/Program.cs
@@ -21,13 +21,10 @@ namespace USEncoder
             var sjis = ConvertEncoding(Encoding.GetEncoding(932), str);
             var sjis_bytes = Encoding.GetEncoding(932).GetBytes(sjis);
             var utf8_bytes = ToEncoding.ToUTF8(sjis_bytes);
-            var utf8 = Encoding.UTF8.GetString(utf8_bytes);
             var clone = ConvertEncoding(Encoding.UTF8, str);
             var clone_bytes = Encoding.UTF8.GetBytes(clone);
 
-
             Console.WriteLine("{0}, {1}", utf8_bytes.Length, clone_bytes.Length);
-            if (utf8_bytes.Length != clone_bytes.Length) throw new IndexOutOfRangeException("長さが合ってない");
             for (int i = 0; i < utf8_bytes.Length; i++)
             {
                 Console.WriteLine("{0}, {1}, {2}", utf8_bytes[i], clone_bytes[i], utf8_bytes[i] == clone_bytes[i]);
@@ -37,7 +34,7 @@ namespace USEncoder
         static string ConvertEncoding(Encoding dest, string str)
         {
             byte[] str_bytes = Encoding.Unicode.GetBytes(str);
-            byte[] to_bytes = Encoding.Convert(Encoding.ASCII, dest, str_bytes);
+            byte[] to_bytes = Encoding.Convert(Encoding.Unicode, dest, str_bytes);
             return dest.GetString(to_bytes);
         }
 
@@ -49,7 +46,6 @@ namespace USEncoder
             var clone_bytes = Encoding.GetEncoding(932).GetBytes(clone);
 
             Console.WriteLine("{0}, {1}", sjis_bytes.Length, clone_bytes.Length);
-            if (sjis_bytes.Length != clone_bytes.Length) throw new IndexOutOfRangeException("長さが合ってない");
             for (int i = 0; i < sjis_bytes.Length; i++)
             {
                 Console.WriteLine("{0}, {1}, {2}", sjis_bytes[i], clone_bytes[i], sjis_bytes[i] == clone_bytes[i]);
